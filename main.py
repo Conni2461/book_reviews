eff28dc7ce36a8b56c59e97c1c37bae8236437d9
Simon Hauser <Simon-Hauser@outlook.de>
2021-11-20 10:22:14 +0100

    split books table

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
-from sqlalchemy import create_engine, Column, String, Integer, Float, exc
+from sqlalchemy import create_engine, Column, Date, String, Integer, Float, exc, and_
 from sqlalchemy.orm import Session, declarative_base
 
 import requests
 
 from selectorlib import Extractor
 import requests
-import json
 from time import sleep
+
 # Create an Extractor by reading from the YAML file
 def scrape(url, file):
     e = Extractor.from_yaml_file(file)
     headers = {
-        'authority': 'www.amazon.com',
-        'pragma': 'no-cache',
-        'cache-control': 'no-cache',
-        'dnt': '1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'none',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-dest': 'document',
-        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        "authority": "www.amazon.com",
+        "pragma": "no-cache",
+        "cache-control": "no-cache",
+        "dnt": "1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "sec-fetch-site": "none",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-dest": "document",
+        "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
     }
     # Download the page using requests
     r = requests.get(url, headers=headers)
@@ -38,21 +38,59 @@ Base = declarative_base()
 Base.metadata.create_all(engine)
 session = Session(engine)
 
-NYT_KEY = "TODO"
+NYT_KEY = "diFVGXgg4M9rd0ee3sUDSXCx2L32tHOY"
 
 if NYT_KEY == "TODO":
     print("Don't forget to put in your NYT_KEY")
     exit(1)
 
+
 def getList(date, name_list):
-    response = requests.get(f"https://api.nytimes.com/svc/books/v3/lists/{date}/{name_list}.json?api-key={NYT_KEY}")
+    response = requests.get(
+        f"https://api.nytimes.com/svc/books/v3/lists/{date}/{name_list}.json?api-key={NYT_KEY}"
+    )
     json = response.json()
     if "results" in json:
         results = json["results"]
         return results["previous_published_date"], results["books"]
     return None, None
 
-def getAll():
+
+def scrapeAmazon():
+    books = (
+        session.query(Amazon)
+        .filter(and_(Amazon.rating == None, Amazon.url != None))
+        .all()
+    )
+    for book in books:
+        amazon = scrape(book.url, "definitions/amazon.yml")
+        print(amazon, book.url)
+        if amazon != None:
+            rating = amazon["rating"]
+            if rating != None:
+                book.rating = rating.split()[0]
+            reviewCount = amazon["reviewCount"]
+            if reviewCount != None:
+                book.review_count = reviewCount.split()[0].replace(",", "")
+            session.commit()
+
+
+def scrapeGoodreads():
+    books = session.query(Goodreads).filter(Goodreads.rating == None).all()
+    for book in books:
+        good = scrape(
+            f"https://www.goodreads.com/search?q={book.isbn}",
+            "definitions/goodreads.yml",
+        )
+        print(good, book.isbn)
+        if good != None:
+            book.rating = good["rating"]
+            book.rating_count = good["ratingCount"]
+            book.review_count = good["reviewCount"]
+            session.commit()
+
+
+def getAllNYT():
     cursor = "current"
     while True:
         sleep(6)
@@ -69,44 +107,38 @@ def getAll():
             score = 15 - book["rank"] + 1
             try:
                 amazon_url = book["amazon_product_url"]
-                b = Book(isbn=book["primary_isbn13"], title=book["title"], author=book["author"], score=score, publisher=book["publisher"], book_image=book["book_image"], amazon_product_url=amazon_url)
+                b = NYT(
+                    isbn=book["primary_isbn13"],
+                    title=book["title"],
+                    author=book["author"],
+                    score=score,
+                    publisher=book["publisher"],
+                    book_image=book["book_image"],
+                    amazon_product_url=amazon_url,
+                )
                 session.add(b)
+                session.add(Amazon(isbn=book["primary_isbn13"], url=amazon_url))
+                session.add(Goodreads(isbn=book["primary_isbn13"]))
+                session.add(GoogleBooks(isbn=book["primary_isbn13"]))
                 session.commit()
-                if amazon_url != None:
-                    amazon = scrape(book["amazon_product_url"], "definitions/amazon.yml")
-                    if amazon != None:
-                        rating = amazon["rating"]
-                        if rating != None:
-                            b.amazon_rating = rating.split()[0]
-                        reviewCount = amazon["reviewCount"]
-                        if reviewCount != None:
-                            b.amazon_review_count = reviewCount.split()[0].replace(",", "")
-                        session.commit()
-
-                good = scrape(f"https://www.goodreads.com/search?q={book['primary_isbn13']}", "definitions/goodreads.yml")
-                if good != None:
-                    b.goodreads_rating = good["rating"]
-                    b.goodreads_rating_count = good["ratingCount"]
-                    b.goodreads_review_count = good["reviewCount"]
-                    session.commit()
 
             except exc.IntegrityError:
                 session.rollback()
-                b = session.query(Book).get(book["primary_isbn13"])
+                b = session.query(NYT).get(book["primary_isbn13"])
                 b.score += score
                 session.commit()
 
+
 def exportToCsv():
-    books = session.query(Book).all()
+    books = session.query(NYT).all()
     with open("books.csv", "w") as f:
         for book in books:
             f.write(f"{book.isbn}\n")
 
 
-class Book(Base):
-    __tablename__ = "Books"
+class NYT(Base):
+    __tablename__ = "NYT"
 
-    # New York Times API
     isbn = Column(String, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     author = Column(String, nullable=False)
@@ -115,21 +147,54 @@ class Book(Base):
     book_image = Column(String, nullable=True)
     amazon_product_url = Column(String, nullable=True)
 
-    # TODO
-    # Google books api
-    # descriptions
-    # genre
+    def __repr__(self) -> str:
+        return f"Book(isbn={self.isbn!r}, title={self.title!r}, author={self.author!r}, score={self.score!r})"
 
-    amazon_rating = Column(Float, nullable=True)
-    amazon_review_count = Column(Integer, nullable=True)
 
-    goodreads_rating = Column(Float, nullable=True)
-    goodreads_rating_count = Column(Integer, nullable=True)
-    goodreads_review_count = Column(Integer, nullable=True)
+class GoogleBooks(Base):
+    __tablename__ = "GoogleBooks"
+
+    isbn = Column(String, primary_key=True, nullable=False)
+    title = Column(String, nullable=True)
+    author = Column(String, nullable=True)
+    description = Column(String, nullable=True)
+    average_rating = Column(Float, nullable=True)
+    rating_count = Column(Integer, nullable=True)
+    language = Column(Integer, nullable=True)
+    page_count = Column(Integer, nullable=True)
+    publisher = Column(String, nullable=True)
+    published_date = Column(Date, nullable=True)
 
     def __repr__(self) -> str:
         return f"Book(isbn={self.isbn!r}, title={self.title!r}, author={self.author!r}, score={self.score!r})"
 
+
+class Amazon(Base):
+    __tablename__ = "Amazon"
+
+    isbn = Column(String, primary_key=True, nullable=False)
+    rating = Column(Float, nullable=True)
+    review_count = Column(Integer, nullable=True)
+    url = Column(String, nullable=True)
+
+    def __repr__(self) -> str:
+        return f"Book(isbn={self.isbn!r}, title={self.title!r}, author={self.author!r}, score={self.score!r})"
+
+
+class Goodreads(Base):
+    __tablename__ = "Goodreads"
+
+    isbn = Column(String, primary_key=True, nullable=False)
+    rating = Column(Float, nullable=True)
+    rating_count = Column(Integer, nullable=True)
+    review_count = Column(Integer, nullable=True)
+
+    def __repr__(self) -> str:
+        return f"Book(isbn={self.isbn!r}, title={self.title!r}, author={self.author!r}, score={self.score!r})"
+
+
 Base.metadata.create_all(engine)
-# getAll()
+# getAllNYT()
 # exportToCsv()
+# scrapeAmazon()
+# scrapeGoodreads()
